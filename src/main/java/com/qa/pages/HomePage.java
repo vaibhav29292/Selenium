a7100a0a069beb067e9c106c12d47583ea0f0886
vaibhav <vaibhav29292@gmail.com>
2021-12-09 16:43:50 +0530

    first git commit

---
+++ src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class HomePage {
+
+	public void homepage(){
+		System.out.println("homepage");
+	}
+}
---
+++ src/main/java/com/qa/utils/TestUtil.java
@@ -0,0 +1,8 @@
+package com.qa.utils;
+
+public class TestUtil {
+
+	public void testutil(){
+		System.out.println("testutils");
+	}
+}
